a67857ffaa6484cbfc1743a66c23f4409efd9c4d
RaizaCirne <raizabraz93@gmail.com>
2022-10-25 17:57:20 -0300

    docs: modificar linha no exercicio

--- PythonExercicios/ex003.py
+++ PythonExercicios/ex003.py
@@ -15,7 +15,7 @@ print('A soma vale', s)
 
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
-s = (n1 + n2)
+s = n1 + n2
 print('A soma entre os números {} e {} vale {}'.format(n1, n2, s))
 
 #Comentários sobre o código:
