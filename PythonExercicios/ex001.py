0733a5705b07af1bbd34525b6cfaa6fa2a9bcf03
RaizaCirne <raizabraz93@gmail.com>
2022-10-25 14:49:28 -0300

    docs: adicionar pasta exercicios

---
+++ PythonExercicios/ex001.py
@@ -0,0 +1,10 @@
+#Desafio 001
+#Crie um programa que escreva "Olá, Mundo" na tela.
+
+#1ª Sugestão
+print('Olá, Mundo!')
+
+
+#2ª Sugestão -> Programa com uma variável
+msg = 'Olá, Mundo!'
+print(msg)
