180dabd2ce4f3383a783f184295fe5bcb4ee8028
RaizaCirne <raizabraz93@gmail.com>
2022-10-25 17:47:36 -0300

    docs: renomear arquivo

--- PythonExercicios/ex003.py
+++ PythonExercicios/ex003.py
@@ -16,7 +16,7 @@ print('A soma vale', s)
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = (n1 + n2)
-print('A soma vale {}'.format(s))
+print('A soma entre os números {} e {} vale {}'.format(n1, n2, s))
 
 #Comentários sobre o código:
 
